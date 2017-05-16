8f41404eda851bee77379511536b5fbd2be1633b
Anwesh43 <anweshthecool0@gmail.com>
2017-05-16 18:39:56 +0530

    using parameters in beego

--- beego_get.go
+++ beego_get.go
@@ -7,5 +7,8 @@ func main() {
 	beego.Get("/test", func(ctx *context.Context) {
 		ctx.Output.Body([]byte("Hello world"))
 	})
+	beego.Get("/test/:name", func(ctx *context.Context) {
+		ctx.Output.Body([]byte("hello " + ctx.Input.Param("name")))
+	})
 	beego.Run()
 }
